f0b7e2a386d5b6b057d6d732454bebb62b26e36a
chackerm <chackerm@microsoft.com>
2018-11-12 15:48:20 -0800

    Part 1: Ticking Clock

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import moment from "moment";
 
-ReactDOM.render(
-  <div>Hello, World!</div>,
-  document.getElementById('root')
-);
+// Rendering once immediately.
+render();
+
+// Rendering repeatedly on a 1 second interval.
+setInterval(() => {
+  render();
+}, 1000);
+
+function render() {
+  // Using the "moment" module to format the current time.
+  const timeString = moment().format("h:mm:ss a");
+
+  ReactDOM.render(
+    // Using a fragment to render more than two top level nodes.
+    <React.Fragment>
+      <span>Current Time = </span>{timeString}
+    </React.Fragment>,
+    document.getElementById('root')
+  );
+}
